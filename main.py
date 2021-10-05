ecfb978e4724c386e833413e2869140df387fc3e
Choroh <allmy66@mail.ru>
2021-10-05 20:39:18 +0400

    First add files

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+Netology
+Import. Module. Package.
+Занятие 1. Домашнее задание.
+04.10.21
+"""
+
+from datetime import date
+import application.salary
+import application.people
+
+
+def main():
+    application.salary.calculate_salary()
+    application.people.get_employees()
+    print(date.today().strftime("%m.%d.%Y"))
+
+
+if __name__ == '__main__':
+    main()
+
+
